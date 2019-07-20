8526eb4d4fcd1aed7a9cdb36b4021f2e265213c7
HlKun <1104736831@qq.com>
2019-07-20 17:24:03 +0800

    helloworld

---
+++ helloworld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .ConfigureServices(svcs => svcs.AddMvc())
+                .Configure(app => app.UseMvc())
+                .Build()
+                .Run();
+        }
+    }
+
+    public class HelloController:Controller
+    {
+        [HttpGet("/hello/{name}")]
+        public IActionResult SayHello(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
